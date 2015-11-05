e0c3454fc656300d5016f1cf0f7e98d360c8b119
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2015-11-05 16:17:02 +0100

    make frontend load with django when using index_django.html

---
+++ js/startup_django.js
@@ -0,0 +1,19 @@
+﻿require.config(requirejsconfig);
+
+//define some global paths
+var basePath = {
+	data: 'data/',
+	img: 'images/',
+	api: 'https://interpreteer.herokuapp.com/'
+};
+
+require(['jquery', 'knockout-3.3.0', 'serializejson', 'app/Backend', 'app/Main', 'domReady!'], function ($, ko) {
+
+	//register modules globally
+	window.$ = $;
+	window.ko = ko;
+
+	var app = new App();
+
+	app.run();
+});
