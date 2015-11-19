198e3a4d397dab9dc23b25d1f5413c4bdfb76785
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-11-19 18:29:53 +0100

    Add API endpoints for development and production

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,7 @@
 import DRFAdapter from 'ember-django-adapter/adapters/drf';
+import ENV from "interpreteer-ember/config/environment";
 
 export default DRFAdapter.extend({
-  host: 'http://localhost:8000',
+  host: ENV.APP.host,
   namespace: 'api'
 });
