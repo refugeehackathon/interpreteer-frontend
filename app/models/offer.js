f68823812b416678a1dfd90fb69fae3c113bf1e9
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-11-27 15:29:48 +0100

    Add user model

--- app/models/offer.js
+++ app/models/offer.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
-  user: DS.attr('string')
+  user: DS.belongsTo('user', { async: true })
 });
