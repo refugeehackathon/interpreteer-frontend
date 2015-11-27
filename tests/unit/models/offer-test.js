eba1b7472b70a88340124a8aad260bada06f4617
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2015-11-27 16:14:04 +0100

    fix failing tests

--- tests/unit/models/offer-test.js
+++ tests/unit/models/offer-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('offer', 'Unit | Model | offer', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:user']
 });
 
 test('it exists', function(assert) {
